319fc4ee9d77eaf12e62fa3ea03662b742226be1
huskyhehe <zhou.he1@northeastern.edu>
2023-01-09 23:31:28 -0800

    Add websocket_urlpatterns

--- vibechat/asgi.py
+++ vibechat/asgi.py
@@ -10,13 +10,21 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 import os
 
 import django
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
+from chatapp.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vibechat.settings')
 django.setup()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
+
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            websocket_urlpatterns
+        )
+    ),
 })
