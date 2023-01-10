2893cf3b6fde74e31551c5de3e66891cc91688b6
huskyhehe <zhou.he1@northeastern.edu>
2023-01-09 22:13:42 -0800

    Implement django.setup() and ProtocolTypeRouter

--- vibechat/asgi.py
+++ vibechat/asgi.py
@@ -9,8 +9,14 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 
 import os
 
+import django
+from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vibechat.settings')
 
-application = get_asgi_application()
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vibechat.settings')
+django.setup()
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+})
